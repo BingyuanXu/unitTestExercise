390641d1056c2615ec462b74dea03a17e01cb24a
BingyuanXu <xubingyuan123@hotmail.com>
2021-06-12 10:39:54 -0500

    set babel, change common js to ES module

--- jestBasic/math.test.js
+++ jestBasic/math.test.js
@@ -1,13 +1,13 @@
-const { add, minus, multi } = require('./math.js');
-
+//const { add, minus, multi } = require('./math.js');
+import {add, minus, multi} from "./math";
 test('test add 3 + 4', () => {
   expect(add(3,4)).toBe(7);
 })
 
-test('test add 3 + 4', () => {
-  expect(minus(3,4)).toBe(7);
+test('test minus 3 + 4', () => {
+  expect(minus(3,4)).toBe(-1);
 })
 
-test('test add 3 + 4', () => {
-  expect(multi(3,4)).toBe(7);
+test('test multi 3 + 4', () => {
+  expect(multi(3,4)).toBe(12);
 })
